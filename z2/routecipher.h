b7d9b75fe1bf1ff56d5f2574fbd2b35338543955
Rash5813 <anokhin_1515@mail.ru>
2025-12-24 22:10:19 +0300

    Update routecipher.h

--- z2/routecipher.h
+++ z2/routecipher.h
@@ -1,31 +1,32 @@
 #pragma once
-#include <string>
 #include <vector>
+#include <string>
+#include <map>
+#include <locale>
+#include <codecvt>
 #include <stdexcept>
+#include <cwctype>
+#include <iostream>
 
-class RouteCipher {
-private:
-    int columns;
-    
-    // Функция для корректного разделения UTF-8 строки на символы
-    std::vector<std::string> splitUTF8(const std::string& str) const;
-
+class cipher_error : public std::invalid_argument {
 public:
-    // Запрещаем конструктор по умолчанию
-    RouteCipher() = delete;
+    explicit cipher_error(const std::wstring& what_arg);
+    explicit cipher_error(const char* what_arg);
+};
+
+class modAlphaCipher
+{
+private:
+    int key;
+    int getValidKey(const int k);
+    std::wstring toValidText(const std::wstring& s);
+    std::vector<std::vector<wchar_t>> createTable(const std::wstring& text);
+    std::wstring readTableVertical(const std::vector<std::vector<wchar_t>>& table);
+    std::wstring readTableHorizontal(const std::vector<std::vector<wchar_t>>& table);
     
-    // Конструктор с установкой ключа
-    RouteCipher(int key);
-    
-    // Метод шифрования
-    std::string encrypt(const std::string& text) const;
-    
-    // Метод дешифрования
-    std::string decrypt(const std::string& text) const;
-    
-    // Геттер для ключа
-    int getKey() const;
-    
-    // Функция для демонстрации процесса шифрования
-    void demonstrateEncryption(const std::string& text) const;
+public:
+    modAlphaCipher() = delete;
+    modAlphaCipher(const int k);
+    std::wstring encrypt(const std::wstring& open_text);
+    std::wstring decrypt(const std::wstring& cipher_text);
 };
